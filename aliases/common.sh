d11ae3c7f9c00b05fde7110e22455d062b52944c
Victo Penso <vic.penso@gmail.com>
2013-08-29 10:37:03 +0200

    do not init old libvirt-shell-functions; simplified vm alias added

--- aliases/common.sh
+++ aliases/common.sh
@@ -60,7 +60,6 @@ alias tl="tmux list-sessions"
 alias ta="tmux attach-session"
 alias man="MANWIDTH=80 MANPAGER=less man"
 alias npm="nocorrect npm"
-alias vm="nocorrect vm"
 alias less="less -R"
 # List all directories in path
 alias path='echo -e ${PATH//:/\\n}'
