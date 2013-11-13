3885ce6b45d7e78ed1de1bb65964c2634bdceeea
Victor Penso <vic.penso@gmail.com>
2013-11-13 14:26:51 +0100

    a single pandoc alias is enough

--- var/aliases/pandoc.sh
+++ var/aliases/pandoc.sh
@@ -1,2 +1,1 @@
-alias md2html="pandoc --standalone --include-in-header=$SCRIPTS/etc/default/pandoc.css"
-alias m2h="md2html --highlight-style pygments --template $SCRIPTS/var/lib/pandoc.template"
+alias md2html="pandoc --standalone --include-in-header=$SCRIPTS/etc/default/pandoc.css --highlight-style pygments --template $SCRIPTS/var/lib/pandoc.template"
