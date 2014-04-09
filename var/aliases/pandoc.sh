5fe82b786b43d7c45fda0ae7fe36b62f88be577f
Victor Penso <vic.penso@gmail.com>
2014-04-09 09:13:40 +0200

    TOC by default when creating HTML from Markdown

--- var/aliases/pandoc.sh
+++ var/aliases/pandoc.sh
@@ -1,1 +1,1 @@
-alias md2html="pandoc --standalone --include-in-header=$SCRIPTS/etc/default/pandoc.css --highlight-style pygments --template $SCRIPTS/var/lib/pandoc.template"
+alias md2html="pandoc --standalone --include-in-header=$SCRIPTS/etc/default/pandoc.css --highlight-style pygments --toc --template  $SCRIPTS/var/lib/pandoc.template"
