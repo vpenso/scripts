87c2c9516f8b7b7e9bc6bebb77adb7159fb4c12a
Victor Penso <v.penso@gsi.de>
2015-07-02 11:32:40 +0200

    Support for metadata in Pandoc

--- var/aliases/pandoc.sh
+++ var/aliases/pandoc.sh
@@ -1,2 +1,2 @@
-alias m2h="pandoc --standalone --include-in-header=$SCRIPTS/etc/default/pandoc.css --highlight-style pygments --template  $SCRIPTS/var/lib/pandoc.template --read=markdown_github --number-sections"
+alias m2h="pandoc --standalone --include-in-header=$SCRIPTS/etc/default/pandoc.css --highlight-style pygments --template  $SCRIPTS/var/lib/pandoc.template --read=markdown_github+yaml_metadata_block --number-sections"
 alias m2ht="m2h --toc"
