d8461fde59fa16ffc17bb0bc26cd439b7d221f31
Victor Penso <vpenso@localhost>
2015-07-31 11:52:37 +0200

    Handy functions for Slurm RMS

--- var/aliases/slurm.sh
+++ var/aliases/slurm.sh
@@ -1,25 +1,139 @@
+# Copyright 2015 Victor Penso
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-function slurm-partitions {
-  sinfo --format "%9P  %6g %10l %5w %5D %13C %N"
+
+alias s=slurm
+
+# Catch all to shorten access ti slurm* functions 
+function slurm {
+  case $1 in
+    n|nodes)
+      shift
+      case $1 in
+        u|users)
+          slurm-node-users 
+          ;;
+        d|draining)
+          sinfo -t drain,draining,drained -R
+          ;;
+        *)
+          sinfo -N
+          ;;
+      esac
+      ;;
+    q|queue)
+      slurm-queue
+      ;;
+    r|report)
+      shift
+      case $1 in
+        a|accounts)
+          sreport cluster accountutilizationbyuser start=$(date --date="90 days ago" +"%Y-%m-%d") tree
+          ;;
+        *)
+          sreport user top start=$(date --date="90 days ago" +"%Y-%m-%d") 
+          ;;
+      esac
+      ;;
+    j|jobs)
+      shift
+      case $1 in
+        c|completed)
+          slurm-jobs-completed
+          ;;
+        f|failed)
+          slurm-jobs-failed
+          ;;
+        p|pending)
+          slurm-jobs-pending
+          ;;
+        t|time)
+          slurm-jobs-time
+          ;;
+        *)
+          slurm-jobs-running
+          ;;
+      esac
+      ;;
+    *)
+      help
+      ;;
+  esac
 }
 
-function slurm-job {
-  
+function help {
+echo "slurm (j)obs  [(c)ompleted|(f)ailed|(p)ending|(t)ime] 
+      (n)odes [(d)raining|(u)sers]
+      (r)eport [(a)ccounts]
+      (q)ueue"
+
+}
+
+
+function slurm-node-users() {
+  squeue \
+    --states RUNNING \
+    --sort 'N' \
+    --format '%12N %10u %2t %3h' \
+    | uniq -c 
 }
 
 
 function slurm-jobs-pending {
-  squeue -o '%.7i %.9Q %.9P %.8j %.8u %.8T %.10M %.11l %.8D %.5C %R' \
+  squeue -o '%.9Q %.9P %.8u %.8T %.10M %.8Dx%5C' \
          -S '-p' \
-         --state=pending
+         --state=pending \
+         | uniq -c
 }
 
+function slurm-jobs-running {
+  squeue \
+    --states RUNNING \
+    --format '%20S %11M %9P %8u %6g %10T %11l' \
+    | sort -k 1 \
+    | uniq -f 2 -c \
+    | tac
+}
+
+function slurm-queue {
+  squeue \
+    --all \
+    --sort '-T,P,u,m,C,D,l,t' \
+    --format '%9P %8u %6g %9T %.4Cx%5D %7m %11l %8f %6b %6q %8W' \
+    | uniq -c
+}
+
+function slurm-jobs-time() {
+  squeue \
+    --all \
+    --sort '-M,T' \
+    --format '%11M %11l %8T %8u %6P %6a %20S' \
+    | uniq -c
+}
+
+# Print a list of jobs in a failed state fro the Slurm accounting
 function slurm-jobs-failed {
-  sacct --state failed \
-        --allusers \
-        --starttime $(date --date="3 days ago" +"%Y-%m-%d") \
-        --allocations \
-        --format jobid,user,nnodes,ncpus,exitcode,elapsed,start,end 
+  sacct \
+    --noheader --allocations --allusers \
+    --starttime $(date --date="14 days ago" +"%Y-%m-%d") \
+    --state failed,node_fail,timeout \
+    --format end,user,account,state,exitcode \
+    | sort -k 1 \
+    | uniq -f 1 -c
+  # Sort by first column for jobs in chronological order
+  # Ignore date in first column for matching adjacent lines
 }
 
 function slurm-jobs-completed {
@@ -28,5 +142,4 @@ function slurm-jobs-completed {
         --starttime $(date --date="7 days ago" +"%Y-%m-%d")\
         --format end,user,nnodes,ncpus,nodelist,elapsed \
         --allocations
-  
 }
