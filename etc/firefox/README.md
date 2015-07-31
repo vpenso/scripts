087f02babc5e240e9b03da4231f9850ecd1447db
Victor Penso <v.penso@gsi.de>
2015-07-31 16:27:22 +0200

    Privacy aware user profile for Firefox

---
+++ etc/firefox/README.md
@@ -0,0 +1,3 @@
+
+Select one of the Firefox `*.js` profiles and link it to `~/.mozilla/firefox/*/user.js`.
+
---
+++ var/aliases/firefox.sh
@@ -0,0 +1,3 @@
+# Always select a user profile before launching the browser
+alias ff='firefox -profilemanager -no-remote'
+# Allow multiple instances of Firefox
