b986ce203036f9ab4bfcc7b0e30925d979e2198c
Victor Penso <v.penso@gsi.de>
2015-08-04 13:24:37 +0200

    Distinguish different pandoc templates for markdown to HTML

--- var/aliases/pandoc.sh
+++ var/aliases/pandoc.sh
@@ -1,2 +1,24 @@
-alias m2h="pandoc --standalone --include-in-header=$SCRIPTS/etc/default/pandoc.css --highlight-style pygments --template  $SCRIPTS/var/lib/pandoc.template --read=markdown_github+yaml_metadata_block --number-sections"
-alias m2ht="m2h --toc"
+
+function pandoc-html-article() {
+  pandoc \
+    --standalone \
+    --toc \
+    --include-in-header=$SCRIPTS/etc/default/pandoc.css \
+    --highlight-style pygments \
+    --template  $SCRIPTS/var/lib/pandoc/html-article.template \
+    --read=markdown_github+yaml_metadata_block \
+    "$@"
+}
+
+function pandoc-html-book() {
+  pandoc \
+    --standalone \
+    --toc \
+    --include-in-header=$SCRIPTS/etc/default/pandoc.css \
+    --highlight-style pygments \
+    --template  $SCRIPTS/var/lib/pandoc/html-book.template \
+    --read=markdown_github+yaml_metadata_block \
+    --number-sections \
+    "$@"
+}
+
