f8c9089d53cf0a471837b1b7c4b9a5ea0edd82e4
Victor Penso <v.penso@gsi.de>
2015-11-13 14:54:40 +0100

    Fix redirection of play() command

--- var/aliases/common.sh
+++ var/aliases/common.sh
@@ -67,6 +67,6 @@ alias m=music
 alias mute="amixer --quiet set Master toggle"
 alias volume="amixer --quiet set Master"
 
-play() { mpg123 $1 2&>1 1>/dev/null & ; disown }
+play() { mpg123 $1 2>&- 1>&- & ; disown }
 stop() { killall mpg123 }
 
