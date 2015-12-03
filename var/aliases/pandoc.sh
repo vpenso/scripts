252cb8794c4c1d4d94a80c1d3a2a3287b793d79d
Victor Penso <v.penso@gsi.de>
2015-12-03 16:04:40 +0100

    Light CSS template for Pandoc generated HTML pages

--- var/aliases/pandoc.sh
+++ var/aliases/pandoc.sh
@@ -3,7 +3,7 @@ function pandoc-html-article() {
   pandoc \
     --standalone \
     --toc \
-    --include-in-header=$SCRIPTS/etc/default/pandoc.css \
+    --include-in-header=$SCRIPTS/var/lib/pandoc/light.css \
     --highlight-style pygments \
     --template  $SCRIPTS/var/lib/pandoc/html-article.template \
     --read=markdown_github+yaml_metadata_block \
