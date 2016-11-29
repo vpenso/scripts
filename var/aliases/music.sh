f9c7c1ef28ebb86d6c762e13fe8405c6721f7de5
Victor Penso <v.penso@gsi.de>
2016-11-29 12:02:30 +0100

    show song played if no argument given

--- var/aliases/music.sh
+++ var/aliases/music.sh
@@ -2,5 +2,20 @@ alias m=music
 alias mute="amixer --quiet set Master toggle"
 alias volume="amixer --quiet set Master"
 
-play() { mpg123 $1 2>&- 1>&- & ; disown }
+##
+# Play a single song given as the first argument
+#
+play() { 
+  # show the song played
+  if [[ $# -eq 0 ]] ; then
+    ps -C mpg123 -o command=
+  # start playing song
+  else
+    mpg123 $1 2>&- 1>&- & ; disown 
+  fi
+}
+
+##
+# Stop playing a song
+#
 stop() { killall mpg123 }
