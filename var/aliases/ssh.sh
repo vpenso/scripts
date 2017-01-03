7fc3c83685e712a2036290009ad5cb476ca67ba3
Victor Penso <v.penso@gsi.de>
2017-01-03 11:31:23 +0100

    Remove clutter

--- var/aliases/ssh.sh
+++ var/aliases/ssh.sh
@@ -1,5 +1,5 @@
 #
-# Copyright 2013-2015 Victor Penso
+# Copyright 2013-2017 Victor Penso
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -14,39 +14,18 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-
 alias se=ssh-exec
 alias sc=ssh-cmd
 alias ss='noglob ssh-sync'
 alias sr='ssh -l root'
-
-# Use archive mode by default
-alias rsync='rsync -avzrtp'
-
-
-# less secure but faster
-alias ssh-fast-encrypt='ssh -C4c arcfour,blowfish-cbc'
-
-# Agent forwarding
-alias ssh-agent-forward='ssh -A'
-
-# Spy on the SSH traffic
-alias ssh-snoop='tcpdump -lnn -i any port ssh and tcp-syn'
-
-# Compression by default for remote mounts
-alias sshfs='sshfs -C'
-
-# Omit checking of target host key
+      shuttle() { sshuttle -r --dns $1 0/0 } # Launch SSH proxy
+alias rsync='rsync -avzrtp'  # Use archive mode by default
+alias ssh-fast-encrypt='ssh -C4c arcfour,blowfish-cbc' # less secure but faster
+alias ssh-snoop='tcpdump -lnn -i any port ssh and tcp-syn' # Spy on the SSH traffic
+alias sshfs='sshfs -C' # Compression by default for remote mounts
 alias ssh-no-checks='ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'
 alias scp-no-checks='scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'
-
 # Generate key-pairs, use `-f <PATH>` to provide the location
-alias ssh-keygen-ecdsa="ssh-keygen -t ecdsa -b 521 "
+alias ssh-keygen-ecdsa='ssh-keygen -t ecdsa -b 521 '
 alias ssh-keygen-rsa='ssh-keygen -t rsa -b 4096'
 alias ssh-keygen-no-password="ssh-keygen -q -t rsa -b 2048 -N ''"
-
-# Launch SSH proxy
-function shuttle() { sshuttle -r --dns $1 0/0 }
-
-
-
