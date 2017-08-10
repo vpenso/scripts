05c1fe1876f13d231401efd243e2edf188935557
Victor Penso <v.penso@gsi.de>
2017-08-10 14:30:08 +0200

    youtube-dl...

--- var/aliases/youtube.sh
+++ var/aliases/youtube.sh
@@ -1,5 +1,5 @@
-# install packages youtube-dl libav-tools
-
+# apt install youtube-dl libav-tools
+## get the latest version from
 # https://github.com/rg3/youtube-dl
 
 alias youtube2mp3='noglob youtube-dl --extract-audio --audio-format mp3'
