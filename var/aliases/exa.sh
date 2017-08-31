e996c1a4fdb3a9e9acc82fc7938a791fe3cac80e
Victor Penso <v.penso@gsi.de>
2017-08-31 09:09:17 +0200

    exa aliases

---
+++ var/aliases/exa.sh
@@ -0,0 +1,8 @@
+##
+# exa is a modern replacement for ls:
+#  
+# * Download the binary from https://the.exa.website/
+
+alias el='exa -l --git'
+alias eG='exa -lG --git'
+alias eT='exa -lT --git --group-directories-first -@ -L 2' 
