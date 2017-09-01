c53c34bb38581b60ba606c3b4fd26b213c423d98
Victor Penso <v.penso@gsi.de>
2017-09-01 14:13:45 +0200

    Slurmd test configuration

--- var/chef/roles/debian/slurm/slurmd.rb
+++ var/chef/roles/debian/slurm/slurmd.rb
@@ -7,6 +7,23 @@ run_list(
 default_attributes(
   package: ['slurmd'],
   systemd_unit: {
+    'etc-slurm.mount': {
+      content: '
+        [Unit]
+        Description=Mount SLURM configuration
+        Wants=network-online.target
+        After=network-online.target
+        [Mount]
+        What=lxrm01.devops.test:/etc/slurm
+        Where=/etc/slurm-llnl
+        Type=nfs
+        Options=ro,nosuid
+        TimeoutSec=10s
+        [Install]
+        WantedBy=multi-user.target
+      ',
+      action: [:create, :enable, :start]
+    },
     'slurmd.service': { action: [:enable]  }
   }
 )
