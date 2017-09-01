3f444612a034ac75fd3a0cd1ea0047f3f076f44d
Victor Penso <v.penso@gsi.de>
2017-09-01 12:33:44 +0200

    Slurmctld test configuration

--- var/chef/roles/debian/slurm/slurmctld.rb
+++ var/chef/roles/debian/slurm/slurmctld.rb
@@ -2,8 +2,15 @@ name 'slurmctld'
 description 'slurmctld'
 run_list(
   'recipe[base]',
-  'role[munged]'
+  'role[slurm_nfs]',
+  'role[munged]',
+  'role[slurmdbd]'
 )
 default_attributes(
+  package: ['slurmctld'],
+  link: { '/etc/slurm': { to: '/etc/slurm-llnl' } },
+  systemd_unit: {
+    'slurmctld.service': { action: [:enable] }
+  }
 )
 
--- var/chef/roles/debian/slurm/slurmd.rb
+++ var/chef/roles/debian/slurm/slurmd.rb
@@ -1,55 +1,8 @@
-name "execution_node"
-description "Cluster Execution Node"
-run_list( 
-  'recipe[sys::boot]',
-  'recipe[sys::apt]',
-  'recipe[sys::accounts]',
-  'recipe[sys::file]',
-  'recipe[sys::link]',
-  'recipe[sys::mount]'
+name 'slurmd'
+description 'SLURM execution node'
+run_list(
+  'recipe[base]'
 )
 default_attributes(
-  sys: {
-    boot: {
-      params: [
-        'cgroup_enable=memory',
-        'swapaccount=1'
-      ]
-    },
-    apt: {
-      packages: [ 
-        'dbus',
-        'libpam-systemd',
-        'slurmd',
-        'stress'
-      ]
-    },
-    accounts: {
-      spock: { uid: 1111 },
-      sulu: { uid: 1112 },
-      kirk: { uid: 1113 },
-      uhura: { uid: 1114 }
-    },
-    file: {
-      '/etc/munge/munge.key' => { content: '030340d651edb16efabf24a8c080d4b7' }
-    },
-    link: {
-      '/etc/systemd/system/multi-user.target.wants/munge.service' => { to: '/lib/systemd/system/munge.service' }
-    },
-    mount: {
-      '/etc/slurm-llnl' => {
-        device: 'lxrm01.devops.test:/etc/slurm-llnl',
-        fstype: 'nfs',
-        options: ['ro','nosuid'],
-        action: [ :mount, :enable ]
-      },
-      '/network' => {
-        device: 'lxrm01.devops.test:/network',
-        fstype: 'nfs',
-        options: ['rw','nosuid'],
-        action: [ :mount, :enable ]
-      }
-    }
-  }
 )
 
