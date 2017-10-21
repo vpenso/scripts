84985fe403e44bf99389809894bc989fcc3eb5c0
Victor Penso <vic.penso@gmail.com>
2017-10-21 19:42:17 +0200

    gksu...

--- var/aliases/firefox.sh
+++ var/aliases/firefox.sh
@@ -7,8 +7,3 @@ alias fj='firejail firefox -no-remote 2>&- & ; disown'
 # Always select a user profile before launching the browser
 alias ff='firefox -profilemanager -no-remote 2>&- & ; disown'
 # Allow multiple instances of Firefox
-
-function firefox-as-user() {
-  xhost +
-  su - $1 -c '$(DISPLAY=:0.0 firefox -no-remote &)'
-}
