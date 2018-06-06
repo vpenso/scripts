276a41044f442a712dc34b65dcad008b0b716f94
Victor Penso <vic.penso@gmail.com>
2018-06-06 19:37:39 +0200

    bash prompt added

--- var/aliases/prompt.sh
+++ var/aliases/prompt.sh
@@ -1,5 +1,18 @@
 
-# Switch the ZSH right prompt off
-function norprompt() {
-  export RPROMPT=''
-}
+if [[ "$SHELL" == *"bash" ]]
+then
+	# simple shell prompt
+	export PS1="\n\e[32m\u\e[0m@\e[34m\h\e[0m:\e[31m\w\e[0m -- \#:\j \n\e[37;1m>>>\e[0m "	
+	# \e[<num>m     ASCI escape ANSI code
+	# \u            user name
+	# \h            hostname
+	# \#            current command number
+	# \j            background jobs
+fi
+
+
+if [[ "$SHELL" == *"zsh" ]]
+then
+	# Switch the ZSH right prompt off
+	zsh-norprompt() { export RPROMPT='' ; }
+fi
