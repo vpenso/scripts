1600c57c5c4d938ccf79f11b3fb02597a2c7c7e7
Victor Penso <vic.penso@gmail.com>
2018-06-06 11:35:33 +0000

    ssh packages install

--- var/aliases/ssh.sh
+++ var/aliases/ssh.sh
@@ -1,10 +1,22 @@
-alias ssync='noglob ssh-sync'
+# install require package on ArchLinux bases distributions
+if [ -f /usr/sbin/pacman ]
+then
+	if ! [[ \
+		-f /usr/bin/sshfs && \
+		-f /usr/bin/sshuttle && \
+		-f /usr/bin/rsync && \
+		-f /usr/bin/tcpdump
+	     ]]
+	then
+		sudo pacman -Sy --noconfirm sshfs sshuttle rsync tcpdump
+	fi
+fi
+
 alias sr='ssh -l root'
       shuttle() { sshuttle -r --dns $1 0/0 ; } # Launch SSH proxy
 alias rsync='rsync -avzrtp'  # Use archive mode by default
 alias ssh-fast-encrypt='ssh -C4c arcfour,blowfish-cbc' # less secure but faster
 alias ssh-snoop='tcpdump -lnn -i any port ssh and tcp-syn' # Spy on the SSH traffic
-alias sshfs='sshfs -C' # Compression by default for remote mounts
 alias ssh-no-checks='ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'
 alias scp-no-checks='scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'
 # Generate key-pairs, use `-f <PATH>` to provide the location
