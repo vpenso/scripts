d0378f22a208df926e6016d0525b4da3977949ab
Victor Penos <vic.penso@gmail.com>
2018-06-07 08:53:01 +0200

    password tool install on Arch

--- var/aliases/password.sh
+++ var/aliases/password.sh
@@ -1,3 +1,11 @@
+if ! [[ -f /usr/sbin/ccrypt && -f /usr/sbin/xclip ]]
+then
+	if [ -f /usr/sbin/pacman ]
+	then
+		sudo pacman -Sy --noconfirm ccrypt xclip
+	fi
+fi
+
 alias pw=password
 
 # Use `password` on a remote node over SSH, and paste to local clipboard
@@ -6,7 +14,7 @@ alias pw=password
 # - Note: Second password prompt won't be visible
 #
 pwrp() { 
-  ssh -t -F ~/.ssh/ssh_config instance "zsh -i -c 'password show $1'" | tail -n1 | tr -d '\n' | xclip -selection clipboard 
-  # clear the clipboard
-  (sleep 30; echo '' | xclip -selection clipboard) &
+	ssh -t -F ~/.ssh/ssh_config instance "zsh -i -c 'password show $1'" | tail -n1 | tr -d '\n' | xclip -selection clipboard 
+	# clear the clipboard
+	(sleep 30; echo '' | xclip -selection clipboard) &
 }
