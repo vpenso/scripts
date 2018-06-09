c17eed8bce099f1b169b549b83294d42e716adb9
Victor Penso <vic.penso@gmail.com>
2018-06-09 18:43:11 +0200

    clean up, firefox as default in i3, more secure firefox container

--- var/aliases/firefox.sh
+++ var/aliases/firefox.sh
@@ -3,11 +3,42 @@ export BROWSER=firefox
 
 # Allow multiple instances of Firefox
 ff() {
-	(firefox -no-remote 2>&1 1>/dev/null &)
+	(firefox --no-remote 2>&1 1>/dev/null &)
 }
 
 # run Firefox in a container
 ffj() { 
-	(firejail firefox -no-remote 2>&1 1>/dev/null &)
+	(firejail firefox --no-remote 2>&1 1>/dev/null &)
+}
+
+# run firefox even more contained (no sound!)
+ffjs() {
+        (firejail --private \
+		  --private-dev \
+		  --private-tmp \
+		  --private-etc=empty \
+		  --private-srv=empty \
+		  --dns=1.1.1.1 \
+		  --shell=none \
+		  --caps.drop=all \
+		  --nonewprivs \
+		  --protocol=unix,inet,inet6 \
+		  --noroot \
+		  --nogroups \
+		  --nosound \
+		  --notv \
+		  --seccomp \
+		  --netfilter \
+		  --machine-id \
+		  --blacklist=/mnt \
+		  --blacklist=/media \
+		  --blacklist=/opt \
+		  --blacklist=/boot \
+		  --blacklist=/sys \
+		  --quiet \
+		  -- firefox --private \
+		             --no-remote \
+		             2>&1 >/dev/null & \
+	) 2>&1 >/dev/null 
 }
 
