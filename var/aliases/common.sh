3f4260b0e14164aaa985c8982d1e71bbf1dc2551
Victor Penso <vic.penso@gmail.com>
2018-06-09 09:14:25 +0200

    Zsh specific aliases on rc file

--- var/aliases/common.sh
+++ var/aliases/common.sh
@@ -81,15 +81,3 @@ uri-encode() {
 }
 
 calc() { echo "$*" | bc -l ; }
-
-if [[ "$SHELL" == *"zsh" ]]
-then
-	alias -g G='|grep'
-	alias -g GV='|grep -v'   # --invert-match
-	alias -g X='|xargs'
-	alias -g CF1='| cut -d" " -f1'
-	alias -g L='|less'
-	alias -g LT='|less-trim'
-fi
-
-
