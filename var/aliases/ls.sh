fd47bee059de7dfd782873427e37915891bf7fd8
Victor Penso <vic.penso@gmail.com>
2019-03-07 03:39:59 +0100

    simpler ls colors deployment

--- var/aliases/ls.sh
+++ var/aliases/ls.sh
@@ -1,20 +1,29 @@
+
+# delete default configuration if it exists
+[ -f ~/.dir_colors ] && rm ~/.dir_colors
+# read the configuration from this repository
+eval "$(dircolors -b $SCRIPTS/etc/dir_colors/rainbow)"
+
 alias ls='ls -Fh --color=always'   # default to classify, human readable sizes and colors
 alias lS='ls -lS'                  # displays file size in order
 alias l='ls -1'                    # only names, one per line
 alias ll='ls -l'                   # long format
 alias l.='ls -lA -d .*'            # only hidden files
-alias cls='clear ; ls'
-alias td='tree -d'                 # list only dirs 
+alias cls='clear ; ls'             # clear shell and exec ls
+alias td='tree -d'                 # list only directories
 alias t2='tree -L 2'               # max recursive depth of 2 levels
 alias tu='tree -pfughF --du'       # permissions, user, group, sizes 
 
+# if the exa command is installed
 command -v exa >&- && {
         alias el='exa -l --git'
         alias eG='exa -lG --git'
         alias eT='exa -lT --git --group-directories-first -@ -L 2'
 }
 
+# if the `lsd` command is installed
 command -v lsd >&- && {
+        alias ls=lsd
         alias l='lsd -1F --group-dirs first'
         alias l.='lsd -1 --all --group-dirs first'
         alias lt='lsd -F --tree'
