3a7bf9190d002dd785d393a26a375187e8580037
Victor Penso <vic.penso@gmail.com>
2019-03-20 21:03:26 +0100

    vim bits

--- var/aliases/vim.sh
+++ var/aliases/vim.sh
@@ -4,21 +4,20 @@
 # https://github.com/junegunn/vim-plug
 [[ -d ~/.vim/autoload ]] || mkdir -vp ~/.vim/autoload
 [[ -f ~/.vim/autoload/plug.vim ]] || \
-	curl -fLo ~/.vim/autoload/plug.vim \
-		https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+        curl -fLo ~/.vim/autoload/plug.vim \
+                https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 # run PlugInstall! and PlugUpdate!
 
 
 # link to the vim configuration file in this repository
-#
-#
 if [ -z $SCRIPTS ]
 then
-	echo \$SCRIPTS must be set!
+        echo \$SCRIPTS must be set!
 else
-	[[ -f ~/.vimrc ]] || \
-		ln -svf $SCRIPTS/etc/vimrc ~/.vimrc
+        [[ -f ~/.vimrc ]] || \
+                ln -svf $SCRIPTS/etc/vimrc ~/.vimrc
 fi
 
 # do not source vimrc on startup, but run with `noncompatible` option
 alias v0='vim -u NONE -N'
+alias vc=vim-cheat
