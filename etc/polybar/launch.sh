575bfe70565adf4725e2bd581e08ada73764858b
Victor Penso <vic.penso@gmail.com>
2019-03-29 21:11:14 +0100

    working on polybar

--- etc/polybar/launch.sh
+++ etc/polybar/launch.sh
@@ -6,7 +6,8 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
+# required for transparency
+compton &
 polybar top &
 
 echo "Bars launched..."
