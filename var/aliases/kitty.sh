c58cbec612c8514d2ac5bbde5deb06c28a232a9b
Victor Penso <vic.penso@gmail.com>
2019-04-04 14:32:50 +0200

    simple prompt indicator, kitty bits...

--- var/aliases/kitty.sh
+++ var/aliases/kitty.sh
@@ -1,4 +1,8 @@
+# link to the configuration in this terminal
 test -L ~/.config/kitty || {
         rm -rf ~/.config/kitty &>/dev/null
         ln -sv $SCRIPTS/etc/kitty ~/.config/kitty
 }
+
+# display an image, alignment left
+alias icat="kitty +kitten icat --align left"
