22ef5a30c54018e8feef238642dd5a5e5acb57fb
Victor Penso <vic.penso@gmail.com>
2019-04-05 11:59:19 +0200

    youtube resolution aliases

--- var/aliases/youtube.sh
+++ var/aliases/youtube.sh
@@ -3,3 +3,6 @@
 # https://github.com/rg3/youtube-dl
 
 alias yt2mp3='youtube-dl --extract-audio --audio-format mp3'
+alias yt2mp4-480p='youtube-dl -f 135+140'
+alias yt2mp4-720p60='youtube-dl -f 298+140'
+alias yt2mp4-1080p60='youtube-dl -f 299+140'
