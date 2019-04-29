4cc62670c5be6dff3e706b479cb8000f35ef0e63
Victor Penso <vic.penso@gmail.com>
2019-04-29 14:07:26 +0200

    help function to mount Android devices

---
+++ var/aliases/mtp.sh
@@ -0,0 +1,21 @@
+MTPFS_MOUNT_POINT=~/mnt
+
+export MTPFS_MOUNT_POINT
+
+if command -v simple-mtpfs |:
+then
+        function mtpfs() {
+                if [[ $# -eq 0 ]]
+                then
+                        simple-mtpfs -l
+                fi
+                if [[ $# -eq 1 ]]
+                then
+                        simple-mtpfs --device $1 $MTPFS_MOUNT_POINT
+                fi
+                if [[ $# -eq 2 ]]
+                then
+                        simple-mptfs --device $1 $2
+                fi
+        }
+fi
