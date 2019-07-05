47b46216b9df9a0a5723360d88aee081d45df002
Victor Penso <vic.penso@gmail.com>
2019-07-05 10:36:52 +0200

    add ssh tmux alias function

--- var/aliases/ssh.sh
+++ var/aliases/ssh.sh
@@ -1,6 +1,8 @@
 alias sr='ssh -l root'
-      shuttle() { sshuttle -r --dns $1 0/0 ; } # Launch SSH proxy
 alias rsync='rsync -avzrtp'  # Use archive mode by default
+
+function ssh-tmux() {/usr/bin/ssh -t $@ "tmux attach || tmux new";}
+
 alias ssh-fast-encrypt='ssh -C4c arcfour,blowfish-cbc' # less secure but faster
 alias ssh-snoop='tcpdump -lnn -i any port ssh and tcp-syn' # Spy on the SSH traffic
 alias ssh-no-checks='ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'
