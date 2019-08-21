2e1d79e9b911eadb02fd9868bfb07a6a3b5026e2
Victor Penso <vic.penso@gmail.com>
2019-08-21 10:04:37 +0200

    disentangle mpc/mpd config

---
+++ var/aliases/mpc.sh
@@ -0,0 +1,61 @@
+MPC_ALIAS_HELP="\
+@,  crop                Remove songs except current from playlist
++,  next                Play next song in playlist
+-,  prev                Play previous song in playlist
+~                       Remove current song from playlist
+1                       Toggles looping
+a,  add PATH            Adds directory to playlist
+c,  clear               Remove all songs from playlist
+    conf                Show MPD configuration
+d,  delete <num>        Remove songs from playlist
+k,  kill                Kill MPD daemon
+l,  list                List library
+pl, playlist            Show current playlist
+p,  play <num>          Plays playlist
+s,  stop                Stop playing music
+u,  update              Update database
+v,  volume [+-]<num>     Adjust volume between 0-100 (+- for relative numbers)
+"
+
+command -v mpc >&- && {
+
+        alias mpc="mpc --host 127.0.0.1 -p $MPD_PORT"
+
+        function mpc-alias() {
+        
+                local command=$1
+                # remove first argument if present
+                [[ $# -ne 0 ]] && shift
+                # for given command...
+                case "$command" in
+                        add|a)          mpc add $@ ;;
+                        clear|c)        mpc -q clear ;;
+                        conf)           tail -n+1 $MPD_CONF ;;
+                        del|d)          mpc del $@ ;;
+                        kill|k)         killall -u $USER mpd ;;
+                        next|-)         mpc prev ;;
+                        list|l)         mpc ls $@ ;;
+                        play|p)         mpc play $1 ;;
+                        playlist|pl)    mpc --format '[%position%] %file%' playlist ;;
+                        prev|+)         mpc next ;;
+                        stop|s)         mpc stop ;;
+                        update|u)       mpc update ;;
+                        volume|v)       mpc volume $1 ;;
+                        ~)              mpc del 0 ;;
+                        0)              mpc current ;;
+                        1)              mpc repeat ;;
+                        @)              mpc crop ;;
+                        *)              echo -n $MPC_ALIAS_HELP ;;
+                esac
+        
+        }
+
+        alias m=mpc-alias
+
+}
+
+# if the ncurses client is installed
+command -v ncmpc >&- && {
+	alias n="ncmpc --host 127.0.0.1 --port $MPD_PORT"
+}
+
---
+++ var/aliases/mpd.sh
@@ -0,0 +1,29 @@
+
+command -v mpd >&- && {
+
+        MPD_CONF=$SCRIPTS/etc/mpdconf
+        # read the port from the configuration file
+        MPD_PORT=$(grep ^port $MPD_CONF | cut -d'"' -f2)
+        MPD_PLAYLISTS=$HOME/.mpd/playlists
+        MUSIC_DIR=$HOME/music
+        
+        export MPD_PORT \
+               MPD_CONF \
+               MPD_PLAYLISTS \
+               MUSIC_DIR
+
+        if [ -d $MUSIC_DIR ]
+        then
+                # if MPD is not running
+                if ! pgrep -u $USER mpd &>/dev/null
+                then
+                        [[ -d $MPD_PLAYLISTS ]] || mkdir -p $MPD_PLAYLISTS
+                        # start MPD
+                        mpd $MPD_CONF 2>&-
+                        # update music index
+                        mpc -w update
+                        echo mpd started
+                fi
+        fi
+
+}
