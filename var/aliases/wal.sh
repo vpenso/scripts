f232ebf85ff87f2deec8daf0957da466e18bbd5e
Victor Penso <vic.penso@gmail.com>
2019-10-15 19:01:47 +0100

    wal celan function addesd

--- var/aliases/wal.sh
+++ var/aliases/wal.sh
@@ -4,6 +4,11 @@ command -v wal >&- && {
 
         test -L ~/.wallpaper && \
                 wal -e -q -l -i ~/.wallpaper ||:
+
+        function wal-clean() {
+                rm -rvf ~/.cache/wal ~/.config/wal
         }
+
+}
 # install on Debian
 # pip3 install pywal
