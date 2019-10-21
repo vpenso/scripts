0114c0b6fa17a8c535c640cb8b7b05f8acf8c3d3
Victor Penso <vic.penso@gmail.com>
2019-10-21 13:19:16 +0200

    mtpfs bits

--- var/aliases/android.sh
+++ var/aliases/android.sh
@@ -2,23 +2,27 @@ MTPFS_MOUNT_POINT=~/mnt
 
 export MTPFS_MOUNT_POINT
 
+command -v simple-mtpfs >&- && {
 
-if command -v simple-mtpfs |:
-then
-        function mtpfs() {
+        function android-mount() {
                 if [[ $# -eq 0 ]]
                 then
-                        simple-mtpfs -l
+                        simple-mtpfs -l || {
+                                echo Connect, and unlock your phone!
+                        }
                 fi
                 if [[ $# -eq 1 ]]
                 then
                         mkdir -p $MTPFS_MOUNT_POINT >/dev/null
-                        simple-mtpfs --device $1 $MTPFS_MOUNT_POINT
+                        simple-mtpfs --device $1 $MTPFS_MOUNT_POINT && {
+                                echo Mount to $MTPFS_MOUNT_POINT
+                        }
                 fi
                 if [[ $# -eq 2 ]]
                 then
                         simple-mptfs --device $1 $2
                 fi
         }
+
         # unmount with: fusermount -u ~/mnt
-fi
+}
