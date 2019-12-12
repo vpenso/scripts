0dfbec0468ec54c916f409b8449367d9ed464d27
Victor Penso <vic.penso@gmail.com>
2019-12-12 11:25:43 +0100

    move personal git config to etc/

--- var/aliases/git.sh
+++ var/aliases/git.sh
@@ -1,6 +1,10 @@
-if ! [ -f ~/.gitconfig ]
-then
-	$SCRIPTS/bin/git-default-config 'Victor Penso' vic.penso@gmail.com
-fi
+command -v git >/dev/null && {
 
-alias g=git
+        alias g=git
+
+        # configure the git command
+        test -L ~/.gitconfig \
+                || ln -sfv $SCRIPTS/etc/gitconfig ~/.gitconfig
+        test -L ~/.gitignore_global \
+                || ln -sfv $SCRIPTS/etc/gitignore_global ~/.gitignore_global
+}
