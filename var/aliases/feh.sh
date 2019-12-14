c3288eed83727d338097e8491f9d2952fe60ed96
Victor Penso <vic.penso@gmail.com>
2019-12-14 10:12:39 +0100

    improve desktop background management

--- var/aliases/feh.sh
+++ var/aliases/feh.sh
@@ -1,3 +1,17 @@
-alias f='feh --conversion-timeout 1'
-# open images in thumbnail mode
-alias ft='feh -t -E 256 -y 256 -H 1200 -W 1920 -xF'
+# Cf. https://github.com/derf/feh
+#
+# Install on Debian with `apt install -y feh`
+#
+command -v feh >&- && {
+
+        alias f='feh --conversion-timeout 1'
+
+        # open images in thumbnail mode
+        alias ft='feh -t -E 256 -y 256 -H 1200 -W 1920 -xF'
+
+        # set a desktop background
+        alias feh-bg='feh --bg-scale'
+
+        # restore the background in the next session
+        test -f ~/.fehbg && source ~/.fehbg
+}
--- var/aliases/wal.sh
+++ var/aliases/wal.sh
@@ -1,14 +1,25 @@
 # Cf. https://github.com/dylanaraps/pywal
+#
+# install the Python module on Debian
+#       pip3 install pywal
 command -v wal >&- && {
+
         alias walli='wal -l -i'
 
-        test -L ~/.wallpaper && \
-                wal -e -q -l -i ~/.wallpaper ||:
+        # path to a wallpaper used by wal
+        export WAL_WALLPAPER=~/.cache/wall/wallpaper
+        # use the wallpaper if existing
+        test -L $WAL_WALLPAPER && wal -e -q -l -i $WAL_WALLPAPER ||:
 
+        # set a new wallpaper
+        function wal-set() {
+                local image=${1:?Path to image file missing!}
+                ln -s $image $WAL_WALLPAPER
+        }
+
+        # remove the wal configuration
         function wal-clean() {
                 rm -rvf ~/.cache/wal ~/.config/wal
         }
 
 }
-# install on Debian
-# pip3 install pywal
