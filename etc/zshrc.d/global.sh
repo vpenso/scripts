7a6672478241eea457a16e8392b0146cd08fab54
Victor Penso <vic.penso@gmail.com>
2020-01-09 17:22:08 +0100

    modularize Zsh configuration

---
+++ etc/zshrc.d/global.sh
@@ -0,0 +1,8 @@
+## Zsh global aliases ##
+
+alias -g G='|grep'
+alias -g GV='|grep -v'   # --invert-match
+alias -g X='|xargs'
+alias -g CF1='| cut -d" " -f1'
+alias -g L='|less'
+alias -g LT='|less-trim'
---
+++ etc/zshrc.d/history.sh
@@ -0,0 +1,9 @@
+export HISTSIZE=10000
+export SAVEHIST=10000
+export HISTFILE=~/.zsh_history
+setopt append_history
+setopt hist_ignore_dups
+setopt hist_ignore_space
+setopt hist_verify
+setopt inc_append_history
+setopt share_history
