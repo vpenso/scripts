ab54189296182ef07a1fbdde3d82bb83206ffe40
Victor Penso <vic.penso@gmail.com>
2020-01-10 17:12:31 +0100

    bookmarks path curation

--- var/aliases/bookmarks.sh
+++ var/aliases/bookmarks.sh
@@ -1,4 +1,10 @@
-export BOOKMARKS_PATH=$BOOKMARKS_PATH:$SCRIPTS/var/bookmarks
+if test -z $BOOKMARKS_PATH
+then
+        export BOOKMARKS_PATH=$SCRIPTS/var/bookmarks
+elif ! $(echo $BOOKMARKS_PATH | grep -q '\/scripts\/')
+then
+        export BOOKMARKS_PATH=$BOOKMARKS_PATH:$SCRIPTS/var/bookmarks
+fi
 
 # open a bookmark in the default web-browser
 alias bm=bookmarks
