80c95ab40dc3a7f37a0c658910381a5567f6b013
Victor Penso <vic.penso@gmail.com>
2020-02-07 14:04:29 +0100

    6502 bits

--- var/aliases/bc.sh
+++ var/aliases/bc.sh
@@ -1,15 +1,32 @@
-function dec2bin() {
+dec2bin() {
   echo "0b$(echo "obase=2;ibase=10;${1:?Expecting decimal number}" | bc)"
 }
 
-function bin2dec() {
+dec2hex() {
+        local dec=${1:?Expecting decimal number}
+        echo "obase=16;ibase=10;$dec" | bc
+}
+
+bin2dec() {
   echo "obase=10;ibase=2;${1:?Expecting binary number}" | bc
 }
 
-function bin2hex() {
+bin2hex() {
   echo "0x$(echo "obase=16;ibase=2;${1:?Expecting binary number}" | bc)"
 }
 
-function hex2bin() {
-  echo "0b$(echo "obase=2;ibase=16;${1:?Expecting hexa-decimal number}" | bc)"
+hex2bin() {
+        local hex=${1:?Expecting hexa-decimal number}
+        # convert to uppercase
+        hex=$(echo $hex | tr '[a-f]' '[A-F]')
+        # calculate binary value
+        bin=$(echo "obase=2;ibase=16;$hex" | bc)
+        echo "0b$bin"
+}
+
+hex2dec() {
+        local hex=${1:?Expecting hexa-decimal number}
+        # convert to uppercase
+        hex=$(echo $hex | tr '[a-f]' '[A-F]')
+        echo "obase=10;ibase=16;$hex" | bc
 }
