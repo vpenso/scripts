6bd05a3842971030d42f20f6980e0fbd1f68c789
Victor Penso <vic.penso@gmail.com>
2020-11-05 07:40:22 +0100

    load xterm config only if server present

--- var/aliases/xterm.sh
+++ var/aliases/xterm.sh
@@ -1,4 +1,4 @@
-test -f $SCRIPTS/etc/Xresources.d/xterm && \
+test -n "$DISPLAY" && test -f $SCRIPTS/etc/Xresources.d/xterm && \
         xrdb -merge $SCRIPTS/etc/Xresources.d/xterm
 
 alias xterm='xterm -ti vt340 -sh 6'
