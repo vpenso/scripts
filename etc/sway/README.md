07b9721f136f2c1a75b618e5a542a29d53832b25
Victor Penso <vic.penso@gmail.com>
2021-01-10 09:27:41 +0100

    working on the sway status bar

--- etc/sway/README.md
+++ etc/sway/README.md
@@ -1,7 +1,7 @@
 
 
 ```bash
-sudo apt install -y sway xwayland
+sudo apt install -y sway xwayland upower
 # use the configuration within this repository
 source $SCRPITS/var/aliases/sway.sh
 # reload the configuration file
--- etc/sway/status.sh
+++ etc/sway/status.sh
@@ -1,1 +1,10 @@
-date +'%Y-%m-%d %l:%M:%S %p'
+date=$(date +'%Y/%m/%d %H:%M')
+upower=$(upower --show-info $(upower --enumerate | grep BAT) | egrep '(energy:|percentage)' | tr -d ' ' | cut -d: -f2 | tr '\n' ' ')
+
+network=
+for i in $(ip addr | awk '/state UP/ {print $2}' | sed 's/.$//')
+do
+        network="$i:$( ip addr show $i | grep 'inet ' | tr -s ' ' | cut -d' ' -f3)"
+done
+
+echo $network $upower $date
