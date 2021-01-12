01276891185fa33f2c7f30a5bd7a03d18a446830
Victor Penso <vic.penso@gmail.com>
2021-01-12 19:47:45 +0100

    add wireless bits into sway status bar

--- etc/sway/status.sh
+++ etc/sway/status.sh
@@ -1,10 +1,22 @@
 date=$(date +'%Y/%m/%d %H:%M')
 upower=$(upower --show-info $(upower --enumerate | grep BAT) | egrep '(energy:|percentage)' | tr -d ' ' | cut -d: -f2 | tr '\n' ' ')
 
-network=
+ethernet=
+wireless=
 for i in $(ip addr | awk '/state UP/ {print $2}' | sed 's/.$//')
 do
-        network="$i:$( ip addr show $i | grep 'inet ' | tr -s ' ' | cut -d' ' -f3)"
+        case $i in
+        enp*|eth*)
+                ip=$( ip addr show $i | grep 'inet ' | tr -s ' ' | cut -d' ' -f3 )
+                ethernet=$i:$ip
+                ;;
+        wlan*)
+                ip=$( ip addr show $i | grep 'inet ' | tr -s ' ' | cut -d' ' -f3 )
+                essid=$( iwconfig wlan0 | head -n1 | cut -d: -f2 | tr -d '"' | xargs )
+                signal=$( iwconfig wlan0 | grep Signal | cut -d= -f3 | tr -d ' ' )
+                wireless="$i:$essid:$ip:$signal"
+        ;;
+        esac
 done
 
-echo $network $upower $date
+echo "$ethernet $wireless | $upower $date"
