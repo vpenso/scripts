3c760fcf19b9c26caae3cba03d87d6d0b20c5d4b
Victor Penso <vic.penso@gmail.com>
2021-01-14 19:09:10 +0100

    swaybar memory status...

--- etc/sway/status.sh
+++ etc/sway/status.sh
@@ -11,17 +11,19 @@ do
         case $i in
         enp*|eth*)
                 ip=$( ip addr show $i | grep 'inet ' | tr -s ' ' | cut -d' ' -f3 )
-                ethernet="$i | $ip"
+                ethernet="$i|$ip "
                 ;;
         wlan*)
                 ip=$( ip addr show $i | grep 'inet ' | tr -s ' ' | cut -d' ' -f3 )
                 essid=$( iwconfig wlan0 | head -n1 | cut -d: -f2 | tr -d '"' | xargs )
                 signal=$( iwconfig wlan0 | grep Signal | cut -d= -f3 | tr -d ' ' )
-                wireless="$i | $essid | $ip | $signal"
+                wireless="$i|$essid|$ip|$signal"
         ;;
         esac
 done
 
 kbd_layout=$(swaymsg -t get_inputs | grep -i 'active_layout_name' | uniq | cut -d'"' -f4)
 
-echo "$ethernet $wireless -- $percent | $energy -- $kbd_layout | $date"
+memory="$(head -n2  /proc/meminfo | tr -d ' ' | cut -d: -f2 | tr -d 'kB' | tr '\n' '/')kB"
+
+echo "net:$ethernet $wireless -- mem: $memory -- bat: $percent|$energy -- $kbd_layout|$date"
