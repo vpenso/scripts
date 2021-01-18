11d7269d4e50c35cb3251aadaed4b06bcd0bf5ad
Victor Penso <vic.penso@gmail.com>
2021-01-18 12:01:27 +0100

    bits...

--- var/aliases/sxiv.sh
+++ var/aliases/sxiv.sh
@@ -2,19 +2,7 @@
 command -v sxiv >&- && {
 
         function sx() {
-                if [ -d "${@: -1}" ] || [ -h "${@: -1}" ]
-                # if it is a directory
-                then
-                        # recursive search
-                        # open with thumbnail mode
-                        # enable private mode
-                        sxiv -rbtp "$@"
-                # it it is a single image
-                else
-                        # no info bar on the bottom
-                        # enable private mode
-                        sxiv -pb "$@"
-                fi
+                sxiv -tp ${@:-$PWD}
         }
 
         # clean up cache in $HOME
