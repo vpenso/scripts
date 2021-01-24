4ba38ac8d4e95c465884c7c8ca419e15c9551e48
Victor Penso <vic.penso@gmail.com>
2021-01-24 08:48:14 +0100

    sway -- wallpaper help functions, some more floating defaults

--- var/aliases/sway.sh
+++ var/aliases/sway.sh
@@ -1,5 +1,16 @@
+# if Sway is installed
 command -v sway >/dev/null && {
+
+        # load the configuration from this repository
         test -d ~/.config || mkdir -p ~/.config
         test -L ~/.config/sway || \
                 ln -s $SCRIPTS/etc/sway ~/.config/sway
+
+        # Set a background wallpaper
+        function sway-wallpaper() {
+                local img=${1:?Missing image file as first command-line argument}
+                rm ~/.cache/wallpaper
+                ln -s $img ~/.cache/wallpaper
+                swaymsg reload
+        }
 }
