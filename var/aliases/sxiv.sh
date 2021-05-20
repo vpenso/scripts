6a5586ec352f4af535621ac246e7d5039abb64ae
Victor Penso <vic.penso@gmail.com>
2021-05-20 09:36:06 +0200

    fuzzy finder bits...

--- var/aliases/sxiv.sh
+++ var/aliases/sxiv.sh
@@ -8,4 +8,12 @@ command -v sxiv >&- && {
         # clean up cache in $HOME
         test -d ~/.cache/sxiv && rm -rf ~/.cache/sxiv
 
+        function sxe() {
+                sxiv -pt $(
+                        fd --type f '.*' $PWD \
+                        | sort \
+                        | fzf -e -m --bind 'ctrl-a:select-all+accept'
+                )
+        }
+
 } 
