7dd7adf02169443c3bab17afdb95bf2c80d79dcd
Victor Penso <vic.penso@gmail.com>
2021-06-17 13:38:41 +0200

    working on neovim config...

--- var/aliases/nvim.sh
+++ var/aliases/nvim.sh
@@ -11,11 +11,14 @@ command -v nvim >/dev/null && {
 	# create the configuration directory if missing
 	test -d ~/.config/nvim || mkdir -p ~/.config/nvim
 	# link to the configuration within this repository
-	test -L ~/.config/nvim/init.vim || \
-		ln -s $SCRIPTS/etc/nvim/init.vim ~/.config/nvim/init.vim
+#        test -L ~/.config/nvim/init.vim || \
+#		ln -s $SCRIPTS/etc/nvim/init.vim ~/.config/nvim/init.vim
+        test -L ~/.config/nvim/init.lua ||
+                ln -s $SCRIPTS/etc/nvim/init.lua ~/.config/nvim/init.lua
 
         # use the AppImage if present
         test -f $HOME/bin/nvim.appimage && alias nvim=nvim.appimage
         alias nv=nvim
+        test -f $HOME/bin/nvim-nightly.appimage && alias nvn=nvim-nightly.appimage
 
 }
