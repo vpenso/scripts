c25669f712f5864e1a3d2d1a11d082e629c3a033
Victor Penso <vic.penso@gmail.com>
2022-01-04 17:12:25 +0100

    rust bits...

--- src/rust/microbit/led_roulette/README.md
+++ src/rust/microbit/led_roulette/README.md
@@ -1,16 +1,16 @@
 ```bash
 ├── build.rs
 ├── .cargo
-│   └── config
-├── Cargo.toml          # metadata and dependencies
-├── Embed.toml
-├── memory.x            # specify the memory layout of the target device
+│   └── config          # Configure build and linking 
+├── Cargo.toml          # Project metadata and dependencies
+├── Embed.toml          # Cargo embedded configuration
+├── memory.x            # Specify the memory layout of the target device
 └── src
     └── main.rs
 ```
 
-Rust on BBC micro:bit - starting with a blinky  
-<https://flames-of-code.netlify.app/blog/rust-microbit/>
-
 Template for building applications for ARM Cortex-M microcontroller  
 <https://docs.rust-embedded.org/cortex-m-quickstart/cortex_m_quickstart>
+
+Cargo subcommand to work with embedded targets  
+<https://docs.rs/crate/cargo-embed>
--- src/rust/microbit/led_roulette/src/main.rs
+++ src/rust/microbit/led_roulette/src/main.rs
@@ -1,3 +1,4 @@
+#![deny(unsafe_code)]
 #![no_std]
 // Use the core crate, a subset of std that can run on bare metal systems (i.e., systems without OS
 // abstractions like files and sockets).
@@ -5,15 +6,17 @@
 // Instead of the standard main we'll use the entry attribute from the cortex-m-rt crate to define
 // a custom entry point.
 
-use panic_rtt_target as _;
-use rtt_target::{rtt_init_print, rprintln};
-
 use cortex_m_rt::entry;
+use panic_halt as _;
+use microbit as _;
 
-#[entry]
+// The entry point function must have signature fn() -> !; this type indicates that the function
+// can't return -- this means that the program never terminates.
+#[entry] 
 fn main() -> ! { 
-    rtt_init_print!();
-    rprintln!("Hello World");
+    let _y;
+    let x = 42;
+    _y = x;
+    // infinite loop; just so we don't leave this stack frame
     loop {}
-    // function can't return -- this means that the program never terminates
 } 
