eeda6ad27aa4e0e2910a488caddb3d5c4449713f
Victor Penso <vic.penso@gmail.com>
2022-01-07 09:09:49 +0100

    rust structs

--- src/rust/rectangle/src/main.rs
+++ src/rust/rectangle/src/main.rs
@@ -26,19 +26,25 @@ impl Rectangle {
 }
 
 fn main() {
+
+    // instance of a struct
     let big_rectangle = Rectangle {
         width: 30,
         height: 50
     };
 
+    // print debug information, call a method from a struct
     println!("{:?} with area {}", big_rectangle, big_rectangle.area());
 
+    // create another instance from using a method
     let small_rectanlge = Rectangle::square(20);
+    // compare two rectangles
     println!("{:?} can hold big rectanlge? {}", 
         small_rectanlge,
         small_rectanlge.can_hold(&big_rectangle)
     );
 
+    // yet another instance
     let similar_rectangle = Rectangle {
         width: 100,
         ..big_rectangle
