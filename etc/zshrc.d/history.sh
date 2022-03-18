83b0a8d87651507f2a12ad467f9f190a1e9ab4d8
Victor Penso <vic.penso@gmail.com>
2022-03-18 07:01:57 +0100

    work on zsh history configuration...

--- etc/zshrc.d/history.sh
+++ etc/zshrc.d/history.sh
@@ -1,9 +1,39 @@
-export HISTSIZE=10000
-export SAVEHIST=10000
+# location of the history file
 export HISTFILE=~/.zsh_history
+# number of commands that are loaded into memory
+export HISTSIZE=100000
+# number of commands that are stored 
+export SAVEHIST=10000
+
+# Sessions will append their history list to the history file, rather than
+# replace it
 setopt append_history
+
+setopt hist_expire_dups_first
 setopt hist_ignore_dups
-setopt hist_ignore_space
+setopt hist_ignore_all_dups
+setopt hist_find_no_dups
+setopt hist_save_no_dups
+
 setopt hist_verify
-setopt inc_append_history
+
+# This option both imports new commands from the history file, and also causes
+# your typed commands to be appended to the history file
 setopt share_history
+
+# Ignore command with leading white-space
+setopt hist_ignore_space
+
+
+##
+# Show the configuration of the shell history
+#
+zsh-history-config() { 
+        printenv | grep HIST
+        setopt kshoptionprint && setopt | grep hist | sort
+}
+
+zsh-history-erase() { 
+        local HISTSIZE=0
+        rm $HISTFILE
+}
