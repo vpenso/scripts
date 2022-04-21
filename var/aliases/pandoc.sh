09bf82ebea0a06f81e081a2489f462287fc6e6f9
Victor Penso <vic.penso@gmail.com>
2022-04-21 13:24:35 +0200

    bits...

--- var/aliases/pandoc.sh
+++ var/aliases/pandoc.sh
@@ -2,14 +2,16 @@ export PANDOC_CSS=light
 export PANDOC_OPTS=
 
 function pandoc-md2html() {
-        local file=${1:?Specify markdown input file}
-        local ofile=${1%.*}.html
-        pandoc --standalone \
-               --self-contained \
-               --highlight pygments \
-               --css $SCRIPTS/var/lib/pandoc/$PANDOC_CSS.css \
-               --output $ofile \
-               $PANDOC_OPTS \
-               $file
-        echo $ofile
+        for file in $@
+        do
+                local ofile=${file%.*}.html
+                pandoc --standalone \
+                       --self-contained \
+                       --highlight pygments \
+                       --css $SCRIPTS/var/lib/pandoc/$PANDOC_CSS.css \
+                       --output $ofile \
+                       $PANDOC_OPTS \
+                       $file
+                echo $ofile
+        done
 }
