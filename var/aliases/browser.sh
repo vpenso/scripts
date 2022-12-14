7bf8a8afbd30470461dcabc9ca3e9970f00bb093
Victor Penso <vic.penso@gmail.com>
2022-12-14 07:20:35 +0100

    browser setting...

--- var/aliases/browser.sh
+++ var/aliases/browser.sh
@@ -1,46 +1,11 @@
-# make Firefox the default browser
-export BROWSER=${BROWSER:-firefox}
 
-# Allow multiple instances of Firefox
-ff() {
-	(firefox --no-remote 2>&1 1>/dev/null &)
-}
+if command -v brave-browser >/dev/null ; then
+        alias brave=brave-browser
+        # set as default browser
+        export BROWSER=${BROWSER:-brave-browser}
 
-# run Firefox in a container
-ffj() { 
-	(firejail \
-                firefox --no-remote 2>&1 >/dev/null \
-        &) 2>&1 >/dev/null
-}
-
-# run firefox even more contained (no sound!)
-ffjs() {
-        (firejail --private \
-		  --private-dev \
-		  --private-tmp \
-		  --private-etc=empty \
-		  --private-srv=empty \
-		  --dns=1.1.1.1 \
-		  --shell=none \
-		  --caps.drop=all \
-		  --nonewprivs \
-		  --protocol=unix,inet,inet6 \
-		  --noroot \
-		  --nogroups \
-		  --nosound \
-		  --notv \
-		  --seccomp \
-		  --netfilter \
-		  --machine-id \
-		  --blacklist=/mnt \
-		  --blacklist=/media \
-		  --blacklist=/opt \
-		  --blacklist=/boot \
-		  --blacklist=/sys \
-		  --quiet \
-		  -- firefox --private \
-		             --no-remote \
-		             2>&1 >/dev/null & \
-	) 2>&1 >/dev/null 
-}
+# Firefox typically installed by default
+else
+        export BROWSER=${BROWSER:-firefox}
+fi
 
