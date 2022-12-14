ef7cc5e22a02595c86537e8c5710d9e4969c4a02
Victor Penso <vic.penso@gmail.com>
2022-12-14 08:09:42 +0100

    configure tmux...

---
+++ var/aliases/tmux.sh
@@ -0,0 +1,13 @@
+
+# if tmux is in $PATH
+if command -v tmux >/dev/null ; then
+
+        # install tmux plugin manager...
+        test -d ~/.tmux/plugins/tpm \
+                || git clone -q https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+        # link to the configuration within this repository
+        test -L ~/.tmux.conf \
+                || ln -s $SCRIPTS/etc/tmux/tmux.conf ~/.tmux.conf
+
+fi
