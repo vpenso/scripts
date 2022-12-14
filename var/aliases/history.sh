b3b2b7c7f878507d3a0352bc0aaa95b8bec4d9ec
Victor Penso <vic.penso@gmail.com>
2022-12-14 11:06:03 +0100

    adjust bits for the hstr command

--- var/aliases/history.sh
+++ var/aliases/history.sh
@@ -1,10 +1,13 @@
 
 if command -v hstr >/dev/null
 then
-      alias hh=hstr                                # hh to be alias for hstr
-      setopt histignorespace                       # skip cmds w/ leading space from history
+      alias '?'=hstr
+      setopt histignorespace
       export HSTR_CONFIG=hicolor,help-on-opposite-side
-      bindkey -s "\C-r" "\C-a hstr -- \C-j"        # bind hstr to Ctrl-r (for Vi mode check doc)
+      export HSTR_PROMPT="> "
+
+      # bind Ctrl-r
+      #bindkey -s '^R' 'hstr^M'
 else
       alias '?'='fc -li 1'                         # list history with time stamp
       alias '?5'='history -5'                      # list last five entries in history
