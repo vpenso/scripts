85297492b315964f3cac9d0ab1103b139ffe5666
Victor Penso <vic.penso@gmail.com>
2023-01-11 14:46:31 +0100

    clean up

--- var/aliases/music.sh
+++ var/aliases/music.sh
@@ -31,24 +31,3 @@ pulse-restart() {
         pulseaudio --start
 }
 
-##
-# Play a single song given as the first argument
-#
-play() { 
-        # show the song played
-        if [[ $# -eq 0 ]]
-        then
-                ps -C ffplay -o command=
-        # start playing song
-        else
-                # part of the ffmpeg package
-                ( ffplay -loglevel quiet -nodisp $1 & ) \
-                        2>&1 >/dev/null
-        fi
-}
-
-##
-# Stop playing a song
-#
-stop() { killall -s KILL ffplay ; }
-
