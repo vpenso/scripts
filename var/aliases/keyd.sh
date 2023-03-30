20f8ebcc7d049045c5911acb21e13ebe3048ee1e
Victor Penso <vic.penso@gmail.com>
2023-03-30 17:44:11 +0200

    keyd bits...

--- var/aliases/keyd.sh
+++ var/aliases/keyd.sh
@@ -3,5 +3,7 @@ if test -d /etc/keyd
 then
         test -L /etc/keyd/default.conf ||
                echo "Keyd configuration file missing in /etc/keyd" 
+
+        alias keyd-restart="sudo systemctl restart keyd"
 fi
 
