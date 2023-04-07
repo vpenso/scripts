5847fcdae51f989b653b010a4409affa95cf7168
Victor Penso <vic.penso@gmail.com>
2023-04-07 18:21:03 +0200

    diplay images in the terminal using kitty

--- var/aliases/kitty.sh
+++ var/aliases/kitty.sh
@@ -1,5 +1,12 @@
+# if Kitty terminal emulator is installed
 command -v kitty >/dev/null && {
+
+        # reference to the configuration in this repository
         test -d ~/.config/kitty && rm -rf ~/.config/kitty
         test -L ~/.config/kitty ||
                ln -s $SCRIPTS/etc/kitty ~/.config/kitty
+
+        # display images in the terminal cf. https://sw.kovidgoyal.net/kitty/kittens/icat
+        alias icat="kitty +kitten icat --align left"
+
 }
