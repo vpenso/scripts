035b651627727a2aabeaa470d082ba50783efb86
Victor Penso <vic.penso@gmail.com>
2023-04-07 18:07:18 +0200

    update for ruby 3+, make sure an empty directory is remove for the kitty config...

--- var/aliases/kitty.sh
+++ var/aliases/kitty.sh
@@ -1,4 +1,5 @@
 command -v kitty >/dev/null && {
+        test -d ~/.config/kitty && rm -rf ~/.config/kitty
         test -L ~/.config/kitty ||
                ln -s $SCRIPTS/etc/kitty ~/.config/kitty
 }
