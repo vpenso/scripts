ae9d4b7751a7ff3b49dac7ab69de1c6f1e611ff1
Victor Penso <vic.penso@gmail.com>
2023-04-11 06:52:43 +0200

    propagate terminfo over ssh when using kitty

--- var/aliases/kitty.sh
+++ var/aliases/kitty.sh
@@ -6,6 +6,9 @@ command -v kitty >/dev/null && {
         test -L ~/.config/kitty ||
                ln -s $SCRIPTS/etc/kitty ~/.config/kitty
 
+        # required to propagate terminfo ...rrror opening terminal: xterm-kitty
+        test "$TERM" = "xterm-kitty" && alias ssh="kitty +kitten ssh"
+
         # display images in the terminal cf. https://sw.kovidgoyal.net/kitty/kittens/icat
         alias icat="kitty +kitten icat --align left"
 
