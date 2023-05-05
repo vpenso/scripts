d93c7ef66146649367bd1c89c0c9caa35091ca5a
Victor Penso <vic.penso@gmail.com>
2023-05-05 07:13:08 +0200

    add a second prefix to tmux...

--- var/aliases/kitty.sh
+++ var/aliases/kitty.sh
@@ -7,7 +7,12 @@ command -v kitty >/dev/null && {
                ln -s $SCRIPTS/etc/kitty ~/.config/kitty
 
         # required to propagate terminfo ...rrror opening terminal: xterm-kitty
-        test "$TERM" = "xterm-kitty" && alias ssh="kitty +kitten ssh"
+        if test "$TERM" = "xterm-kitty"
+        then
+                ssh() { 
+                        kitty +kitten ssh $@
+                }
+        fi
 
         # display images in the terminal cf. https://sw.kovidgoyal.net/kitty/kittens/icat
         alias icat="kitty +kitten icat --align left"
