e47c855a012a881359389b7703e504a4acf1916f
Victor Penso <vic.penso@gmail.com>
2023-08-25 10:12:47 +0200

    linux media aliases...

--- var/aliases/youtube-dl.sh
+++ var/aliases/youtube-dl.sh
@@ -1,16 +1,1 @@
-command -v youtube-dl >&- && {
-        alias yt2mp3='youtube-dl --extract-audio --audio-format mp3'
-}
-
-function youtube() {
-        if [[ $# -eq 1 ]]
-        then
-                youtube-dl -F "$@"
-                return
-        fi
-
-        if [[ $# -eq 2 ]]
-        then
-                youtube-dl -f $2 "$1"
-        fi
-}
+alias youtube='yt-dlp -f "bv*+ba/b"'
