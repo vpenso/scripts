4fbf5466db395105a678e669b6326b3fd9826e9f
Victor Penso <vic.penso@gmail.com>
2024-02-20 13:07:42 +0100

    bits...

--- var/aliases/browser.sh
+++ var/aliases/browser.sh
@@ -2,7 +2,7 @@
 if command -v brave-browser >/dev/null ; then
         alias brave=brave-browser
         # set as default browser
-        export BROWSER=${BROWSER:-brave-browser}
+#        export BROWSER=${BROWSER:-brave-browser}
 
 # Firefox typically installed by default
 else
