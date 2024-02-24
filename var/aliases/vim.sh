56e64797799e4402da9ad90ecb6b6529d9e99bdb
Victor Penso <vic.penso@gmail.com>
2024-02-24 13:57:50 +0100

    vim bits...

--- var/aliases/vim.sh
+++ var/aliases/vim.sh
@@ -1,42 +1,21 @@
-# if NeoVim is installed
-if command -v nvim >/dev/null
-then
 
-	# create the configuration directory if missing
-	test -d ~/.config/nvim || mkdir -p ~/.config/nvim
+# Vi is installed in close to all Linux environments by default
+export EDITOR=vi
+alias v=vi
 
-        # install vim-plug if missing
-        test -d ~/.config/nvim/autoload || {
-                mkdir -p ~/.config/nvim/autoload
-                curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
-                        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-        }
-
-	# link to the configuration within this repository
-        test -L ~/.config/nvim/init.vim || \
-		ln -s $SCRIPTS/etc/nvim/init.vim ~/.config/nvim/init.vim
-#        test -L ~/.config/nvim/init.lua ||
-#                ln -s $SCRIPTS/etc/nvim/init.lua ~/.config/nvim/init.lua
-
-        
-        # use the AppImage if present
-        test -f $HOME/bin/nvim.appimage \
-                && alias nvim=nvim.appimage
-
-        export EDITOR=nvim
-        alias v=nvim
-        alias v0='nvim --clean'
-
-elif command -v vim >/dev/null
-then
-
-        export EDITOR=nvim
+# …if Vim is installed us it instead of classic Vi
+command -v vim >/dev/null && {
+        export EDITOR=vim
         alias v=vim
         alias v0='vim --clean'
+}
 
-else
+# if NeoVim is installed by AppImage in home-directory
+test -f $HOME/bin/nvim.appimage && alias nvim=nvim.appimage
 
-        export EDITOR=vi
-        alias v=vi
+# if NeoVim is installed
+command -v nvim &>/dev/null && {
+        export EDITOR=nvim
+        alias nv=nvim
+}
 
-fi
