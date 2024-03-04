8835f1e384d46ce8e098ecd7786750727acce24a
Victor Penso <vic.penso@gmail.com>
2024-03-04 08:39:10 +0100

    bits...

--- var/aliases/neofetch.sh
+++ var/aliases/neofetch.sh
@@ -3,7 +3,7 @@ command -v neofetch >/dev/null && {
         # default path to the user configuration
         config=$HOME/.config/neofetch/config.conf
         # link to this repository if missing
-        test -L $config || ln -s $SCRIPTS/etc/neofetch/config.conf $config
+        test -L $config || ln -fs $SCRIPTS/etc/neofetch/config.conf $config
 
         # speed up neofetch by storing its output for one hour
         tmp=/tmp/$USER-neofetch.$(date +%Y%m%dT%H)
