caa0f1b85149fa28f3dc5dfd2864b5d252803b9b
Victor Penso <vic.penso@gmail.com>
2024-05-29 14:41:28 +0200

    clean up aliases...

--- var/aliases/wikipedia.sh
+++
@@ -1,8 +0,0 @@
-function @wikipedia() {
-  local lang=${1:-"en"}
-  shift 2>-
-  $BROWSER "http://$lang.wikipedia.org/w/index.php?search=$(uri-encode $@)"
-}
-
-alias @w="@wikipedia en"
-alias @w.de="@wikipedia de"
