24374f0ca6bdd1e0fdd982692eb1e6c3fd98b040
Victor Penso <vic.penso@gmail.com>
2025-05-14 17:18:37 +0200

    add kubectl alias

--- var/aliases/kubernetes.sh
+++ var/aliases/kubernetes.sh
@@ -1,8 +1,13 @@
 command -v kubectl >/dev/null && {
+        
+        alias k=kubectl
 
         # load command completion
 	case "$SHELL" in
-        /bin/bash) source <(kubectl completion bash) ;;
+        /bin/bash) 
+                source <(kubectl completion bash) 
+                complete -o default -f __start_kubectl k
+                ;;
         /bin/zsh) source <(kubectl completion zsh) ;;
         esac
 
