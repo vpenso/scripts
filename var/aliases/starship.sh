8d87645846777e98395c1db4100cdf6be19f39a9
Victor Penso <vic.penso@gmail.com>
2025-06-03 07:27:46 +0200

    basic prompt as fallback

--- var/aliases/starship.sh
+++ var/aliases/starship.sh
@@ -1,5 +1,14 @@
-command -v starship >/dev/null && {
+if command -v starship >/dev/null
+then
     test -L ~/.config/starship.toml \
         || ln -sf $SCRIPTS/etc/starship.toml ~/.config/starship.toml
     eval "$(starship init bash)"
-}
+else
+    # simple default shell prompt
+    export PS1="\n\e[32m\u\e[0m@\e[34m\h\e[0m:\e[31m\w\e[0m \n󱆃 󰜴 "
+    # \e[<num>m     ASCI escape ANSI code
+    # \u            user name
+    # \h            hostname
+    # \#            current command number
+    # \j            background jobs
+fi
